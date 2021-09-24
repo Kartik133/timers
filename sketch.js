5650a3bcd94624c653e11a941e9b26c06ba4cc26
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-09-24 18:26:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,20 @@
-var counter = 0,timeLeft = 60;
+var counter = 0,timeLeft = 60,t;
 
 function convertSeconds(s) {
    var min = floor(s/60);
    var sec = s%60;
-   return nf(min,2)+":"+(sec,2);
+   return nf(min,2)+":"+nf(sec,2);
 }
 
 function draw() {
    createCanvas(400,400);
    
-   textAlign(CENTER);
-   text(convertSeconds(timeLeft-counter),200,200);
-   
    function timeIt() {
      counter++
      textAlign(CENTER);
-     text(convertSeconds(timeLeft-counter),200,200);
+     t = createElement("h1");
+     t.position(200,200);
+     t.html(convertSeconds(timeLeft-counter),200,200);
    }
    setInterval(timeIt,1000);
 }
