2929eeaf4d766ed32cf624ad78d2f8b6033d22ba
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-09-24 18:59:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var counter = 0,timeLeft = 110;
+var y;
 
 function convertSeconds(s) {
    var min = floor(s/60);
@@ -7,11 +8,20 @@ function convertSeconds(s) {
 }
 
 function setup() {
-   function timeIt() {
-     counter++
-     
+   if(y.value) {
+    function timeIt() {
+     counter++;
      var timer = select("#timer");
      timer.html(convertSeconds(timeLeft-counter));
+    }
    }
+   
+   y = createInput("seconds");
+   y.position(200,200);
+   
    setInterval(timeIt,1000);
+}
+
+function draw() {
+   var v = y.value();
 }
