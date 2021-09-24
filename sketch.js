ff17a5186cc8bc4e1c859d6559c9c6acaf22fa99
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2021-09-24 18:22:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,55 +1,25 @@
-var hor,min,sec;
-var gameState = "start";
-var horInput,minInput,secInput,start;
-var hr,minu,seco;
-var timerHr,timerMn,timerSc;
-var hr3,min3,sec3
+var counter = 0,timeLeft = 60;
+
+function convertSeconds(s) {
+   var min = floor(s/60);
+   var sec = s%60;
+   return nf(min,2)+":"+(sec,2);
+}
 
 function setup() {
    createCanvas(400,400);
+   
+   textAlign(CENTER);
+   text(convertSeconds(timeLeft-counter),200,200);
+   
+   function timeIt() {
+     counter++
+     textAlign(CENTER);
+     text(convertSeconds(timeLeft-counter),200,200);
+   }
+   setInterval(timeIt,1000);
 }
 
 function draw() {
-   if(gameState==="start") {
-     horInput = createInput("00");
-     minInput = createInput("00");
-     secInput = createInput("00");
-     start = createButton("start");
-     horInput.position(200,200);
-     minInput.position(200,250);
-     secInput.position(200,300);
-     start.position(200,350);
-     start.mousePressed(()=>{
-        hor = hour();
-        min = minute();
-        sec = second();
-        horInput.hide();
-        minInput.hide();
-        secInput.hide();
-        start.hide();
-        gameState = "start2";
-     })
-   }
-
-   if(gameState==="start2") {
-      text(hor,150,150);
-      text(min,180,150);
-      text(sec,210,150);
-      horInput.hide();
-      minInput.hide();
-      secInput.hide();
-      start.hide();
-      hr = hour();
-      minu = minute();
-      seco = second();
-      hr3 = horInput.value() + hor;
-      min3 = minInput.value() + min;
-      sec3 = secInput.value() + sec;
-      timerHr = hr3-hr;
-      timerMn = min3-minu;
-      timerSc = sec3-seco;
-      textSize(30);
-
-      text(timerHr + " : " + timerMn + " : " + timerSc,200,200);
-   }
+   
 }
